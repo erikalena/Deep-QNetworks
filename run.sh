0ab860775fe61fdaf9381c1d7f0cc8ec8487e284
Fracesc0rtu <francescortu@live.it>
2023-07-15 19:02:32 +0200

    fix to run the right script

--- run.sh
+++ run.sh
@@ -14,4 +14,4 @@
 conda activate torch
 echo $SLURM_JOB_ID
 # Run the application
-python -m src_code.script.dqn_script --output_log_dir ./results/${SLURM_JOB_ID} --output_checkpoint_dir ./checkpoint/${SLURM_JOB_ID}
+python -m src_code.script.dqn_new --output_log_dir ./results/${SLURM_JOB_ID} --output_checkpoint_dir ./checkpoint/${SLURM_JOB_ID}
